f321445ff6ff462e37ead1552260e78d5a75c8e2
Vladimir Sabaev <vladimir8sabaev@mail.ru>
2023-04-27 09:58:13 +0300

    init: start virtual-keyboard task

---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'delevopment';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    static: path.resolve(__dirname, 'src'),
+    port: 5000,
+    open: true,
+    hot: true,
+  },
+  entry: {
+    index: ['@babel/polyfill', './src/index.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      inject: true,
+      chunks: ['index'],
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+
+    new CopyPlugin({
+      patterns: [{ from: 'src/img', to: 'img' }],
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src/keys.json', to: '' }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(c||sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.wav$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'music/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g||webp||gif||png||svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
